a85f45512a5e1ef8e69ebeaa0c08ff19b01b225c
Sea Grapes <natuloid@gmail.com>
2022-11-15 16:23:31 -0800

    commands don't work :/

--- src/js/background.js
+++ src/js/background.js
@@ -1,1 +1,8 @@
-// TODO
+chrome.commands.onCommand.addListener((command) => {
+  // console.log(`Command "${command}" triggered`);
+  if(command !== 'picker') return;
+  console.log('picker');
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, 'picker');
+  });
+});
--- src/js/content.js
+++ src/js/content.js
@@ -2,8 +2,18 @@
 // Append the main script to access the window variable
 // Set the type to module to allow import statements
 
-const script = document.createElement('script');
-script.setAttribute('type', 'module');
-script.setAttribute('src', chrome.runtime.getURL('src/js/main.js'));
-document.documentElement.append(script);
+// const script = document.createElement('script');
+// script.setAttribute('type', 'module');
+// script.setAttribute('src', chrome.runtime.getURL('src/js/main.js'));
+// document.documentElement.append(script);'
 
+console.log('content');
+
+chrome.runtime.onMessage.addListener((req) => {
+  addEventListener('keyup', e => {
+    const eyeDropper = new EyeDropper();
+    eyeDropper.open().then((result) => {
+      console.log(result);
+    })
+  }, { once: true })
+});
