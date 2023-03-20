edd5e2ec827eb5be16ae70b7b811dc02acc0a8c4
Sea Grapes <natuloid@gmail.com>
2023-03-19 16:35:10 -0800

    ,

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,39 @@
 
-const nav = document.querySelector('.nav')
-const activeTab = 0
+// const nav = document.querySelector('.nav')
+// const activeTab = 0
+
+// const buttons = Array.from(document.querySelectorAll('.nav > button'))
+// buttons.clear = function() {
+//   buttons.forEach(e => e.classList)
+// }
+
+// buttons.forEach(button => {
+//   button.addEventListener('click', e => {
+
+//   })
+// })
+
+const tabs = new class {
+  constructor() {
+    this.buttons = Array.from(document.querySelectorAll('.nav > button'))
+    this.tabs = Array.from(document.querySelectorAll('.tabs > div'))
+    this.buttons.forEach((button, i) => {
+      button.addEventListener('click', e => {
+        this.clear()
+        this.activate(i)
+      })
+    })
+
+    this.activate(0)
+  }
+
+  activate(id) {
+    this.buttons[id].classList += 'active'
+    this.tabs[id].style.display = 'block'
+  }
+
+  clear() {
+    this.buttons.forEach(button => button.classList = '')
+    this.tabs.forEach(tab => tab.style.display = 'none')
+  }
+}
